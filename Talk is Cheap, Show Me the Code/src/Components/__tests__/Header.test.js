35911568660cec3df6cb0312c9e5745bfd12d970
MogiliManikanta <you@example.com>
2024-09-17 22:52:56 +0530

    17/09/24

--- Talk is Cheap, Show Me the Code/src/Components/__tests__/Header.test.js
+++ Talk is Cheap, Show Me the Code/src/Components/__tests__/Header.test.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import Header from "../Header";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import appStore from "../../utils/appStore";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -20,4 +20,23 @@ it("Should render the Header Component", () => {
 
   // Assert that the Cart text is in the document
   expect(cartItems).toBeInTheDocument();
+});
+
+it("Should Login and Logout in the Header Component", () => {
+  // Wrap the Header component in the Provider and render it
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // Find the Cart link or button with flexible text matching using regex
+  const login = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(login);
+  const logout = screen.getByRole("button", { name: "LogOut" });
+
+  // Assert that the Cart text is in the document
+  expect(logout).toBeInTheDocument();
 });
